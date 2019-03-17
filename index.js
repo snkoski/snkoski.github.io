d17885c146379c0c989ac38eee0ca3375c244fb7
snkoski <shawnnkoski@gmail.com>
2019-03-17 17:05:56 -0400

    Adding little pictures for different technologies. Responsive menu bar

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-function toggleHamburger() {
-  let x = document.getElementById("topnav");
-  if(x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
+// function toggleHamburger() {
+//   let x = document.getElementById("nav");
+//   if(x.className === "navbar") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "navbar";
+//   }
+// }
+window.onload = function() {
+  const nav = document.querySelector("nav ul")
+  const navToggle = document.querySelector("nav .skip")
+  if (navToggle) {
+    navToggle.addEventListener("click", function(e) {
+      if (nav.className == "open") {
+        nav.className = "close";
+      } else {
+        nav.className = "open";
+      }
+      e.preventDefault();
+    }, false);
   }
 }
+
+
+
+
+//
